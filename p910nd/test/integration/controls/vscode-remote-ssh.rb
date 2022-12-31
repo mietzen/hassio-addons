f455eb6bd34e49be6623fe6dfdc676f31d1ef82e
Nils Stein <social.nst@mailbox.org>
2022-12-31 21:52:44 +0100

    fix p910nd startup and test

--- p910nd/test/integration/controls/vscode-remote-ssh.rb
+++ p910nd/test/integration/controls/vscode-remote-ssh.rb
@@ -1,6 +1,6 @@
 title "p910nd Integration Test"
 
-describe processes(Regexp.new("p910nd")) do
+describe processes(Regexp.new("p9100d")) do
   it { should exist }
   its('users') { should include 'root' }
   its('pids') { should cmp "1"}

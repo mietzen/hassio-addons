73438637f8768d9fe1a7e3de740627e653648f66
Nils Stein <social.nst@mailbox.org>
2022-12-31 22:03:08 +0100

    Fix test regex

--- p910nd/test/integration/controls/vscode-remote-ssh.rb
+++ p910nd/test/integration/controls/vscode-remote-ssh.rb
@@ -1,6 +1,6 @@
 title "p910nd Integration Test"
 
-describe processes(Regexp.new("p9100d")) do
+describe processes(Regexp.new("p910[0n]d")) do
   it { should exist }
   its('users') { should include 'root' }
   its('pids') { should cmp "1"}

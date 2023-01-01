44d153a0096c511c8aea96d0f512585510b4e8b9
Nils Stein <31704359+mietzen@users.noreply.github.com>
2023-01-01 13:49:24 +0100

    refine build pipeline (#10)
    * refine build pipeline

---
+++ p910nd/test/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker run -d --rm \
+    --name ${ADDON} \
+    --platform ${PLATFORM} \
+    "${DOCKER_USER}/hass-addon-${ADDON}:${VERSION}-${ARCH}-${BUILD_NR}"
+
+# Install tools needed for inspect
+docker exec -u 0 ${ADDON} apt-get update
+docker exec -u 0 ${ADDON} apt-get install net-tools procps -y
+
+exit 0
---
+++ vscode-remote-ssh/test/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker run -d --rm \
+    --name ${ADDON} \
+    --platform ${PLATFORM} \
+    -v $(pwd)/test/resources/options.json:/data/options.json \
+    -v $(pwd)/test/resources/bashio-mok:/usr/lib/bashio/bashio.sh \
+    "${DOCKER_USER}/hass-addon-${ADDON}:${VERSION}-${ARCH}-${BUILD_NR}"
+
+# Install tools needed for inspect
+docker exec -u 0 ${ADDON} apt-get update
+docker exec -u 0 ${ADDON} apt-get install net-tools procps -y
+
+exit 0
